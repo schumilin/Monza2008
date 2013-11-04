f146aad89d5b9cb0ff1b836a2298bea298ace65e
zhailin <schumilin@163.com>
2013-11-04 16:10:08 +0800

    big step

--- README.md
+++ README.md
@@ -1,4 +1,55 @@
-monza2008
+Monza 2008
 =========
 
-we have made a big step today for that and I really look forward
+> We have made a big step today for that and I really look forward
+
+## Preload Image jQuery Plugin
+
+###version: 0.5
+ 
+###updated: 2013 - 11- 04
+
+####change log: 
+
+> 改成 jQuery 插件的形式，写法更加人性化。但是在有多个节点处理时写起来比较麻烦。
+
+####Usage:
+```javascript
+    $('.container').preload({
+        fadeIn: false,
+        duration: '500ms'
+    });
+```
+
+property
+value
+default
+example
+fadeIn
+if you need a fade for show
+true
+true || false
+duration
+the duration of the fade
+1s
+'2s' || '2500ms'
+url
+if the node doesn’t has background-image in css
+none
+'http://img.wdjimg.com/ios-landing-page/images/screen.jpg'
+nextImage
+callback function
+none
+[{
+        selector: '.container',
+        fadeIn: true
+}]
+direction
+up down left right
+none
+'left'
+distance
+length (px)
+none
+'300'
+
---
+++ jquery.preload.js
@@ -0,0 +1,65 @@
+/*!
+ * jQuery preload image plugin
+ * Original author: zhailin
+ */
+
+; (function ($, window, document, undefined) {
+    var pluginName = 'preloadImage',
+        defaults = {
+            fadeIn: 'true',
+            duration: '1s',
+            distance: '50'
+        };
+    
+    function Plugin(element, options) {
+        this.element = $(element);
+        this.options = $.extend({}, defaults, options);
+
+        this.init(this.element, this.options);
+    }
+
+    Plugin.prototype.init = function (element, options) {
+        var img = new Image(),
+            imgUrl = options.url || element.css('background-image').slice(4, -1),
+            self = this;
+
+        img.onload = function () {
+            img.onload = null;
+            self.showImage(element, options, img);
+        };
+
+        img.src = imgUrl;
+    }
+
+    Plugin.prototype.showImage = function (element, options, img) {
+
+            options.url && element.css('background-image', 'url(' + options.url + ')');
+            options.direction && element.css(options.direction, '-=' + options.distance);
+
+            if (options.fadeIn) {
+                var asyncShow = function () {
+                    options.direction && element.css(options.direction, '+=' + options.distance);
+
+                    element.css({
+                        'transition': 'all ' + (options.duration),
+                        'opacity': '1'
+                    });
+                }
+                setTimeout(asyncShow, 0);
+            } else {
+                element.show();
+            }
+
+            delete img;
+
+            options.nextImage && $(options.nextImage.selector).preloadImage(options.nextImage);
+    }
+
+    $.fn[pluginName] = function (options) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+            }
+        });
+    }
+})(jQuery, window, document);
